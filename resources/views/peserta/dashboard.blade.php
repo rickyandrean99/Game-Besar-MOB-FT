425cbc2794a45db5b31a984d554180fa4f2dbf3f
rickyandrean99 <ricky.ralph09@gmail.com>
2021-06-27 01:29:07 +0700

    Penentuan struktur halaman

--- resources/views/peserta/dashboard.blade.php
+++ resources/views/peserta/dashboard.blade.php
@@ -5,9 +5,25 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Game Besar MOB FT</title>
+        <link rel="stylesheet" href="{{ asset('assets/css/dashboard-peserta.css') }}">
+        <style>
+        
+        </style>
     </head>
     
     <body>
-        
+        <div id="container">
+            <header>
+            
+            </header>
+            
+            <div id="content">
+            
+            </div>
+
+            <footer>
+            
+            </footer>
+        </div>
     </body>
 </html>
