bf9eb72f689204a2cd835061b01f8965e187ca39
rickyandrean99 <ricky.ralph09@gmail.com>
2021-07-15 01:15:26 +0700

    yes

--- resources/views/admin/round.blade.php
+++ resources/views/admin/round.blade.php
@@ -61,7 +61,7 @@
                 if (ronde < 1) {
                     $('.ronde').html("Game Besar Belum Dimulai");
                     $('#btn-action').attr('disabled', 'disabled');
-                } else if (ronde > 20) {
+                } else if (ronde > 16) {
                     $('.ronde').html("Game Besar Sudah Selesai");
                     $('.sesi').text("");
                     $('.timer').text("");
